93745c8b0a0458406dcb6a389ea8a4ca7fc70e61
LuisFerER <luisfercho123@live.com.mx>
2025-01-23 23:42:00 -0600

    Funcion de SortearAmigo secreto

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 let lista = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado');
 
 function agregarAmigo(){
     let amigo = document.getElementById('amigo').value;
-    console.log(amigo);
     if (amigo === '') {
         alert('Por favor, inserte un nombre.');
     } else {
@@ -14,13 +14,21 @@ function agregarAmigo(){
     }
 }
 
-function agregarLista(amigos){
+function agregarLista(){
     lista.innerHTML = '';
     for(let i = 0; i < amigos.length; i++) {
         let nuevoAmigo = document.createElement('li');
         nuevoAmigo.appendChild(document.createTextNode(amigos[i]));
         lista.appendChild(nuevoAmigo);
     }
+}
+
+function sortearAmigo() {
+    lista.innerHTML = '';
+    let indiceAmigo = Math.floor(Math.random()*amigos.length);
+    let amigoSecreto = document.createElement('li');
+    amigoSecreto.appendChild(document.createTextNode(amigos[indiceAmigo]));
+    resultado.appendChild(amigoSecreto);
 }
 
 function limpiarCaja(){
