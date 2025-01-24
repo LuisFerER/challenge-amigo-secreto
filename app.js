09d4a6b86d70d93a16b3e04125fd175590d2fb4e
LuisFerER <luisfercho123@live.com.mx>
2025-01-23 22:47:18 -0600

    Agregar amigos al array en app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    console.log(amigo);
+    if (amigo === '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        amigos.push(amigo);
+        limpiarCaja();
+    }
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+    return;
+}
