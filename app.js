1ebf765ffa0ccd3371409d7205c376a14c369ba5
LuisFerER <luisfercho123@live.com.mx>
2025-01-23 23:26:59 -0600

    Funcion agregar a la lista

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+let lista = document.getElementById('listaAmigos');
 
 function agregarAmigo(){
     let amigo = document.getElementById('amigo').value;
@@ -8,7 +9,17 @@ function agregarAmigo(){
         alert('Por favor, inserte un nombre.');
     } else {
         amigos.push(amigo);
+        agregarLista(amigos);
         limpiarCaja();
+    }
+}
+
+function agregarLista(amigos){
+    lista.innerHTML = '';
+    for(let i = 0; i < amigos.length; i++) {
+        let nuevoAmigo = document.createElement('li');
+        nuevoAmigo.appendChild(document.createTextNode(amigos[i]));
+        lista.appendChild(nuevoAmigo);
     }
 }
 
