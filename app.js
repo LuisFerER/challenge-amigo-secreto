c2aee46636db3c465d98772991f1d706949da097
LuisFerER <luisfercho123@live.com.mx>
2025-01-25 22:30:44 -0600

    Actualiza el app

--- app.js
+++ app.js
@@ -3,35 +3,53 @@ let amigos = [];
 let lista = document.getElementById('listaAmigos');
 let resultado = document.getElementById('resultado');
 
+/*
+* Funcion para agregar los nombres a la lista de amigos.
+*/
 function agregarAmigo(){
     let amigo = document.getElementById('amigo').value;
     if (amigo === '') {
         alert('Por favor, inserte un nombre.');
     } else {
-        amigos.push(amigo);
-        agregarLista(amigos);
-        limpiarCaja();
+        if (!amigos.includes(amigo)) { // Sin que se repitan los nombres dentro de la lista
+            amigos.push(amigo);
+            agregarLista(amigos);
+            limpiarCaja();
+        }
     }
 }
 
+/*
+* Funcion para agregar la lista de amigos a la lista en html.
+* Mostrandolos en una etiqueta <li>
+*/
 function agregarLista(){
-    lista.innerHTML = '';
+    limpiarContenedores();
     for(let i = 0; i < amigos.length; i++) {
         let nuevoAmigo = document.createElement('li');
         nuevoAmigo.textContent = amigos[i];
         lista.appendChild(nuevoAmigo);
     }
 }
-
+ /*
+* Funcion para sortear el amigo secreto a partir de un indice random.
+ */
 function sortearAmigo() {
-    lista.innerHTML = '';
+    limpiarContenedores();
     let indiceAmigo = Math.floor(Math.random()*amigos.length);
     let amigoSecreto = document.createElement('li');
-    amigoSecreto.textContent = amigos[indiceAmigo];
+    amigoSecreto.textContent = 'El amigo secreto es: ' + amigos[indiceAmigo];
     resultado.appendChild(amigoSecreto);
 }
 
-function limpiarCaja(){
+/*
+* Funcion para limpiar contenedores:
+* Input
+* Listas
+*/
+function limpiarContenedores(){
     document.querySelector('#amigo').value = '';
+    lista.innerHTML = "";
+    resultado.innerHTML = "";
     return;
 }
